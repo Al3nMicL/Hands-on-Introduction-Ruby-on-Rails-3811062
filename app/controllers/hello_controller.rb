8a248c0cbef443f55018d080b8a4a942822a3a29
Al3nMicL <al3nmicl@users.noreply.github.com>
2025-04-22 11:55:16 -0400

    01_03b - creating a controller and the view

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,5 @@
+class HelloController < ApplicationController
+  def index
+    #render json: { message: 'Hello, World!' } # This controller defines a single action, `index`, which responds to HTTP requests
+  end
+end
